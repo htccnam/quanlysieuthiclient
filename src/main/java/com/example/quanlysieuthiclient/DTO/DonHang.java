53769bbd812a7a50635700fb779bfac64debfc41
NorSaH <167691649+NorSaH77@users.noreply.github.com>
2026-05-10 11:50:06 +0700

    add

--- src/main/java/com/example/quanlysieuthiclient/DTO/DonHang.java
+++ src/main/java/com/example/quanlysieuthiclient/DTO/DonHang.java
@@ -10,12 +10,12 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DonHang {
-    private String maDonHang;
-    private String maKH; // Thay cho maKH
-    private String maNV;  // Thay cho maNV
-    private Date NgayGD; // Chuyển sang String để hiển thị đẹp (dd/MM/yyyy)
-    private String PTban;
-    private String PTgiaodich;
-    private double tongTien;
-    private String maKM; // Thay cho maKM
+    private String madonhang;
+    private String makhachhang;
+    private String manhanvien;
+    private String makhuyenmai;
+    private Date ngaylap;
+    private String phuongthucban;
+    private String thanhtoan;
+    private double tongtien;
 }
